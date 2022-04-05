87dfdc4b872daac052dd6c8bdfa21492bb3fa49d
Harish <harikrishnapuram198@gmail.com>
2022-04-05 13:34:07 +0530

    added two assignments folders

---
+++ Assignment1/js/script.js
@@ -0,0 +1,7 @@
+let NotificationBtn = document.getElementById("NotificationBtn");
+let notificationBadge = document.getElementById("notificationBadge");
+let nextPagebtn = document.getElementById("nextPagebtn");
+
+NotificationBtn.addEventListener("click",()=>{
+    notificationBadge.classList.add("d-none");
+})
